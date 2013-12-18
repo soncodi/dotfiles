2688561b5e40da92b4ef83d764f65efedb992475
Alex <asoncodi@gmail.com>
2013-12-18 14:15:36 -0600

    Delete kvm-launch.sh

--- kvm-launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PATH=$PATH:/media/Data/Libraries/android-sdk-linux/sdk/tools
-
-emulator-x86 -avd FastNexus7 -gpu on -qemu -m 2047 -enable-kvm
-
-# eclipse params
-#-qemu -m 2047 -enable-kvm
