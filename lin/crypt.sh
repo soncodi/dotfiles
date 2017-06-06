d2e5b9ee7ed96542fc62fee16b8926ff2f63d647
Alex Soncodi <asoncodi@gmail.com>
2017-06-05 22:48:25 -0500

    Update script.

--- lin/crypt.sh
+++ lin/crypt.sh
@@ -10,13 +10,28 @@ swapon -a
 swapon -s
 cat /proc/swaps
 
-# dm-crypt
-#dd if=/dev/urandom of=crypt.fs bs=1M count=1024
-#cryptsetup luksFormat crypt.fs
-#sudo cryptsetup luksOpen crypt.fs cryptfs
-#sudo mkfs.ntfs --fast --verbose /dev/mapper/cryptfs # no gid/uid via ext3/4
-#sudo mount /dev/mapper/cryptfs cryptfs
+#!/bin/bash
 
-sudo cryptsetup luksOpen crypt.fs cryptfs && \
-  sudo mount -o umask=0022,gid=1000,uid=1000 /dev/mapper/cryptfs cryptfs
-sudo umount cryptfs && sudo cryptsetup luksClose cryptfs
+set -e
+
+device=cryptdev
+fs="/xx/crypt.fs"
+folder="/yy/cryptfs"
+
+if [[ "$1" == "on" ]]; then
+  mkdir -p "$folder"
+  sudo cryptsetup luksOpen "$fs" "$device"
+  sudo mount -o umask=0022,gid=1000,uid=1000 /dev/mapper/"$device" "$folder"
+elif [[ "$1" == "off" ]]; then
+  sudo umount "$folder"
+  sudo cryptsetup luksClose "$device"
+  rmdir "$folder" --ignore-fail-on-non-empty
+elif [[ $1 == "make" && "$2" -gt 0 ]]; then
+  dd if=/dev/urandom of="$fs" bs=1M count="$2"
+  cryptsetup luksFormat --cipher aes-xts-plain64 "$fs"
+  sudo cryptsetup luksOpen "$fs" "$device"
+  sudo mkfs.ntfs --fast --verbose /dev/mapper/"$device" # no gid/uid via ext3/4
+  sudo cryptsetup luksClose "$device"
+else
+  echo "on|off|make [size-mb]"
+fi
