907673fa04052aea5ba34fd3ffd141b652a8ce2c
Alex Soncodi <asoncodi@gmail.com>
2017-06-19 13:21:11 -0500

    Add key.

--- lin/install1.sh
+++ lin/install1.sh
@@ -15,6 +15,7 @@ add-apt-repository -y ppa:byobu/ppa
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
 
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 echo "deb https://deb.nodesource.com/node_7.x xenial main
 deb-src https://deb.nodesource.com/node_7.x xenial main" > /etc/apt/sources.list.d/nodesource.list
 
