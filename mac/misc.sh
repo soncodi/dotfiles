c51784dfc31e100362538f432dcbccbf1ca58e00
Alex Soncodi <asoncodi@gmail.com>
2017-12-29 00:43:14 -0600

    Disable char picker.

--- mac/misc.sh
+++ mac/misc.sh
@@ -2,7 +2,7 @@
 # https://apple.stackexchange.com/questions/10467
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
-
+defaults write -g ApplePressAndHoldEnabled -bool false
 
 # http://osxnotes.net/keybindings.html
 # mkdir -p ~/Library/KeyBindings
