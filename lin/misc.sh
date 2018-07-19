1d8ac4215a51fdfb78f639a0dea92533c761b6a2
Alex Soncodi <asoncodi@gmail.com>
2018-07-19 02:34:37 -0500

    Update misc.

--- lin/misc.sh
+++ lin/misc.sh
@@ -22,18 +22,12 @@ ircs://server.com:9999/?pass=password
 sudo wget -O /lib/firmware/ath10k/QCA6174/hw3.0/firmware-6.bin \
   https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/4.4.1/firmware-6.bin_WLAN.RM.4.4.1-00110-QCARMSWP-1?raw=true
 
-# # ppk
+# key => ppk
 # puttygen key.pem -o key.ppk -O private
 # puttygen key.pem -o key.pub -O public
 
-# # pem
+# putty => key
+# puttygen key.ppk -O private-openssh -o key.key
+
+# pem
 # openssl x509 -in bundle.crt -out bundle.pem -outform PEM
-
-# # scale
-# xrandr --output DP-2 --scale 0.66667x0.66667
-
-# # cert order
-# cat domain_com.crt \
-# COMODORSADomainValidationSecureServerCA.crt \
-# COMODORSAAddTrustCA.crt \
-# AddTrustExternalCARoot.crt > domain.crt
