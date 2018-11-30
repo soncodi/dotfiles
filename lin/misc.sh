b7285613e632726d304f14d7345d3ee2df835149
Alex Soncodi <asoncodi@gmail.com>
2018-11-29 23:56:16 -0600

    Update module.

--- lin/misc.sh
+++ lin/misc.sh
@@ -26,7 +26,10 @@ GTK_IM_MODULE=ibus
 
 # Atheros QCA6174 firmware
 sudo wget -O /lib/firmware/ath10k/QCA6174/hw3.0/firmware-6.bin \
-  https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/4.4.1/firmware-6.bin_WLAN.RM.4.4.1-00110-QCARMSWP-1?raw=true
+  https://github.com/kvalo/ath10k-firmware/raw/master/QCA6174/hw3.0/4.4.1/firmware-6.bin_WLAN.RM.4.4.1-00124-QCARMSWPZ-1
+
+sudo wget -O /lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin \
+  https://github.com/kvalo/ath10k-firmware/raw/master/QCA6174/hw3.0/board-2.bin
 
 # key => ppk
 # puttygen key.pem -o key.ppk -O private
