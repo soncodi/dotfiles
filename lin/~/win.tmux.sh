5f04a867d55c93a78949ce450bc86a1aa55105da
Alex Soncodi <asoncodi@gmail.com>
2019-12-12 16:22:08 -0600

    Update term file.

--- lin/~/win.tmux.sh
+++ lin/~/win.tmux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 PROJ="/home/user/projects"
 
 # 1
@@ -8,15 +10,20 @@ tmux send-keys -t 0.0 "cd $PROJ/p1" C-m C-l
 tmux split -t 0.0 -h -c "$PROJ/p1"
 tmux send-keys -t 0.1 "mongo p1" C-m
 
-# 2
-tmux neww -n "" -c "$PROJ/p2"
-tmux splitw -t 1.0 -h -c "$PROJ/p2"
-tmux send-keys -t 1.1 "gulp" C-m
+# 1
+tmux rename-window ""
+tmux send-keys -t 0.0 "cd $PROJ/p2" C-m C-l
+tmux splitw -t 0.0 -h -c "$PROJ/p2"
+tmux splitw -t 0.1 -v -c "$PROJ/p2"
+tmux send-keys -t 0.1 "npm run p2u" C-m
+tmux send-keys -t 0.2 "npm run p2a" C-m
 
-# 3
+# 2
 tmux neww -n "" -c "$PROJ/p3"
-tmux splitw -t 2.0 -h -c "$PROJ/p3"
-tmux send-keys -t 2.1 "gulp" C-m
+tmux splitw -t 1.0 -h -c "$PROJ/p3"
+tmux splitw -t 1.1 -v -c "$PROJ/p3"
+tmux send-keys -t 1.1 "npm run dev" C-m
+tmux send-keys -t 1.2 "npm run watch" C-m
 
 tmux select-window -t 0
 tmux select-pane -t 0
