ccdd6ef797c4b2ef294e0b5207f19c52f935c574
Alex Soncodi <asoncodi@gmail.com>
2020-12-06 13:13:38 -0600

    Disable nemo.

--- lin/nemo.sh
+++ lin/nemo.sh
@@ -1,19 +1,19 @@
 
-apt install nemo
+# apt install nemo
 
-# add `nemo-desktop` to startup apps
-# dconf-editor /org/nemo/desktop
-gsettings set org.gnome.desktop.background show-desktop-icons false
-xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
-gsettings set org.nemo.preferences date-format 'iso'
-gsettings set org.nemo.preferences default-folder-viewer 'list-view'
-gsettings set org.nemo.desktop show-desktop-icons true
-gsettings set org.nemo.icon-view default-zoom-level 'small'
-gsettings set org.nemo.desktop trash-icon-visible true
-gsettings set org.nemo.desktop use-desktop-grid false
-#gsettings set org.nemo.desktop vertical-grid-adjust 1.0
-#gsettings set org.nemo.desktop horizontal-grid-adjust 1.0
+# # add `nemo-desktop` to startup apps
+# # dconf-editor /org/nemo/desktop
+# gsettings set org.gnome.desktop.background show-desktop-icons false
+# xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+# gsettings set org.nemo.preferences date-format 'iso'
+# gsettings set org.nemo.preferences default-folder-viewer 'list-view'
+# gsettings set org.nemo.desktop show-desktop-icons true
+# gsettings set org.nemo.icon-view default-zoom-level 'small'
+# gsettings set org.nemo.desktop trash-icon-visible true
+# gsettings set org.nemo.desktop use-desktop-grid false
+# #gsettings set org.nemo.desktop vertical-grid-adjust 1.0
+# #gsettings set org.nemo.desktop horizontal-grid-adjust 1.0
 
-# nautilus
-# xdg-mime default nautilus.desktop inode/directory application/x-gnome-saved-search
-# gsettings set org.gnome.desktop.background show-desktop-icons true
+# # nautilus
+# # xdg-mime default nautilus.desktop inode/directory application/x-gnome-saved-search
+# # gsettings set org.gnome.desktop.background show-desktop-icons true
