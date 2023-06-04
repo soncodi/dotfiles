717a55b97d0881df8565c23242e0b04b71206d5d
Alex Soncodi <asoncodi@gmail.com>
2023-06-04 15:23:10 -0500

    Update install.

--- lin/install.sh
+++ lin/install.sh
@@ -1,5 +1,4 @@
 
-# server
 apt install \
   curl \
   build-essential \
@@ -9,7 +8,6 @@ apt install \
   byobu \
   fail2ban
 
-# desktop
 apt install \
   gparted \
   dconf-editor \
@@ -26,31 +24,26 @@ apt purge \
   sosreport
 
 # apt purge \
+#   gnome-remote-desktop \
 #   printer-driver-* \
 #   cups*
 
-# apt purge gnome-remote-desktop
-
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
-
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 #sudo ufw delete allow 443/tcp
 #sudo sed -i '/ufw-before-input.*icmp/s/ACCEPT/DROP/g' /etc/ufw/before.rules
-
 sudo ufw enable
-
+# sudo ufw disable
 sudo ufw status verbose
 sudo ufw status numbered
 cat /var/log/ufw.log
-# sudo ufw disable
 
 # /etc/ssh/sshd_config
 # Port 1234
 # PasswordAuthentication no
 
-# caps => ctrl in terminal
 # /etc/default/keyboard
 XKBOPTIONS="ctrl:nocaps"
 sudo dpkg-reconfigure keyboard-configuration
@@ -88,3 +81,42 @@ mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 npm config set save-prefix='~'
 npm config set send-metrics=false
+
+# # ttf
+# apt install ttf-mscorefonts-installer
+# sudo fc-cache -f -v
+
+# # p4merge
+# cd ~/Desktop/p4v-*
+# sudo mkdir /opt/p4v
+# sudo mv * /opt/p4v
+# sudo ln -s /opt/p4v/bin/p4merge /usr/local/bin/p4merge
+# apt install libxcb-xinerama0
+
+# # FF playback
+# apt install libavcodec-extra
+
+# purge snap
+# sudo apt purge snapd gnome-software-plugin-snap
+# sudo rm -rf /var/cache/snapd/
+# rm -rf ~/snap
+
+# extensions
+# cd /usr/share/glib-2.0 && glib-compile-schemas .
+# /usr/share/gnome-shell/extensions
+# /usr/share/glib-2.0/schemas
+# /usr/share/gnome-shell/modes/ubuntu.json
+# ~/.local/share/gnome-shell/extensions
+# ~/.local/share/glib-2.0/schemas
+
+# # fp
+# sudo apt install libfprint-2-tod1
+# # http://dell.archive.canonical.com/updates/pool/public/libf/libfprint-2-tod1-goodix/
+# sudo dpkg -i libfprint-2-tod1-goodix_0.0.6-0ubuntu1~somerville1_amd64.deb
+# sudo pam-auth-update
+
+# # android udev
+# # https://github.com/M0Rf30/android-udev-rules/blob/master/51-android.rules
+# # /etc/udev/rules.d/51-android.rules
+# sudo usermod -aG plugdev $LOGNAME
+
